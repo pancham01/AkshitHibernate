92cf5c627dc243226fead532b399ed5db4b1f901
pancham01 <pancham9557@gmail.com>
2025-03-03 19:38:08 +0530

    update

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -3,6 +3,10 @@ package com.hibernate;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 import com.hibernate.entity.Employee;
@@ -10,17 +14,26 @@ import com.hibernate.entity.Employee;
 public class App {
 
 	public static void main(String[] args) {
-		Employee e = new Employee( "Nakul", "male", 94944);
+		Employee e = new Employee("Nakul", "male", 94944);
+//		1st 
+//		Configuration cfg = new Configuration().configure("hibernate.cfg1.xml");
+//		SessionFactory sessionFactory = cfg.buildSessionFactory();
+
+//		2nd
+//		SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg1.xml").buildSessionFactory();
 		
-		Configuration cfg = new Configuration().configure("hibernate.cfg1.xml");
-		SessionFactory sessionFactory = cfg.buildSessionFactory();
+//		3rd
+//		StandardServiceRegistry ssr = new StandardServiceRegistryBuilder().configure("hibernate.cfg1.xml").build();
+//		Metadata meta = new MetadataSources(ssr).getMetadataBuilder().build();
+//		SessionFactory sessionFactory = meta.buildSessionFactory();
+		
+//		4th
+		SessionFactory sessionFactory = new MetadataSources(new StandardServiceRegistryBuilder().configure("hibernate.cfg1.xml").build()).getMetadataBuilder().build().buildSessionFactory();
+	
 		Session session = sessionFactory.openSession();
-		Transaction tx = session.beginTransaction();
 		session.save(e);
-		tx.commit();
-		
+		session.beginTransaction().commit();
+
 	}
 
 }
-
-
