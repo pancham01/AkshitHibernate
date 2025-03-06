f56e5736a8beee32e3247449caf9b576c1a173a3
pancham01 <pancham9557@gmail.com>
2025-03-06 18:24:41 +0530

    update

--- src/main/java/com/hibernate/App.java
+++ src/main/java/com/hibernate/App.java
@@ -5,11 +5,6 @@ import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 import org.hibernate.query.Query;
 
 import com.hibernate.config.HibernateConfiguration;
@@ -21,7 +16,7 @@ public class App {
 		Employee e1 = new Employee("Ansh", "male", 94944);
 		Employee e2 = new Employee("John", "male", 94944);
 		Employee e3 = new Employee("Vikas", "male", 94944);
-		Employee e4 = new Employee("Ashish", "male", 94944);
+		Employee e4 = new Employee("Ashish", "male", 194944);
 		Employee e5 = new Employee("Aditi", "female", 94944);
 		Employee e6 = new Employee("Sameer", "female", 94944);
 		SessionFactory sessionFactory = HibernateConfiguration.getSessionFactory();
@@ -30,10 +25,10 @@ public class App {
 //		session.persist(e1);
 //		session.persist(e2);
 //		session.persist(e3);
-//		session.persist(e4);
+		session.persist(e4);
 //		session.persist(e5);
 //		session.persist(e6);
-//		session.beginTransaction().commit();
+		tx.commit();
 
 //		Query query = session.createQuery("from Employee",Employee.class);
 //		List list = query.list();
@@ -44,17 +39,20 @@ public class App {
 //		query.setMaxResults(5);
 //		List list = query.list();
 //		System.out.println(list);
-		
-		Query query = session.createQuery("update Employee set name =:n where id =:i");
-		query.setParameter("n", "Shiv");
-		query.setParameter("i", 3);
-		
-		query.executeUpdate();
-		
-//		Query query1 = session.createQuery("from Employee",Employee.class);
-//		List list = query1.list();
-//		System.out.println(list);
-		tx.commit();
+
+//		Query query = session.createQuery("update Employee set name =:n where id =:i");
+//		query.setParameter("n", "Shiv");
+//		query.setParameter("i", 3);
+//		
+//		query.executeUpdate();
+
+//		Query query = session.createQuery("delete from Employee  where id =3");
+//		query.executeUpdate();
+//		tx.commit();
+		Query query1 = session.createQuery("select max(salary) from Employee");
+
+		System.out.println(query1.list());
+
 		sessionFactory.close();
 	}
 
